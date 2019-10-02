fb928c7f49ca568caeeae60ec2679d8aa2c12e91
pauljbuldak <pauljbuldak@lewisu.edu>
2019-10-02 15:26:47 -0500

    Update files

--- potpourri.js
+++ potpourri.js
@@ -1,3 +1,43 @@
 function DoSomething() {
 	console.log("DoSomething called");
 }
+
+function RollSixSidedDice() {
+	console.log("RollSixSidedDice()");
+	document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
+}
+
+function DoCalculations() {
+    x = document.getElementById("xID").value;
+    y = document.getElementById("yID").value;
+    console.log("x="+x+" and y="+y);
+
+    x = Number(x);
+    y = Number(y);
+
+    sum = x+y;
+    difference = x-y;
+    product = x*y;
+    quotient = x/y;
+    test = "2" + 5;
+
+    mathResults = "sum="+sum+"<br>";
+    mathResults += "difference="+difference+"<br>";
+    mathResults += "product="+product+"<br>";
+    mathResults += "quotient="+quotient+"<br>";
+    mathResults += "test="+test+"<br>";
+    document.getElementById("mathResultsID").innerHTML = mathResults;
+
+    var a = 7;
+    var b = 2;
+    if (a  == b || a > 5) {
+        console.log("123");
+        if (b > 3 && a < 8) {
+            console.log("456");
+        } else {
+            console.log("789");
+        }
+        if (a+b > 9)
+        console.log("10");
+    }
+}
